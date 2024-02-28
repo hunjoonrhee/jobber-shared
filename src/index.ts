6a127367774aeb47d4cdfedf2e7e453dabc3eb2b
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-02-28 20:29:02 +0100

    microservices helper Library

---
+++ src/index.ts
@@ -0,0 +1,89 @@
+export {
+    IAuthPayload,
+    IAuthDocument,
+    IAuthBuyerMessageDetails,
+    IEmailMessageDetails,
+    IAuth,
+    ISignUpPayload,
+    ISignInPayload,
+    IForgotPassword,
+    IResetPassword,
+    IReduxAuthPayload,
+    IReduxAddAuthUser,
+    IReduxLogout,
+    IAuthResponse,
+    IAuthUser
+} from './interfaces/auth.interface';
+export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
+export {
+    IChatBoxProps,
+    IChatBuyerProps,
+    IChatMessageProps,
+    IChatSellerProps,
+    IConversationDocument,
+    IMessageDetails,
+    IMessageDocument,
+} from './interfaces/chat.interface';
+export { IEmailLocals } from './interfaces/email.interface';
+export {
+    ICreateGig,
+    IGigCardItems,
+    IGigContext,
+    IGigInfo,
+    IGigTopProps,
+    IGigViewReviewsProps,
+    IGigsProps,
+    ISelectedBudget,
+    ISellerGig
+} from './interfaces/gig.interface'
+export {
+    IDeliveredWork,
+    IExtendedDelivery,
+    IOffer,
+    IOrderDocument,
+    IOrderEvents,
+    IOrderMessage,
+    IOrderNotifcation,
+    IOrderReview
+} from './interfaces/order.interface';
+export {
+    IRatingCategories,
+    IRatingCategoryItem,
+    IRatingTypes,
+    IReviewDocument,
+    IReviewMessageDetails
+} from './interfaces/review.interface';
+export {
+    IHitsTotal,
+    IPaginateProps,
+    IQueryList,
+    IQueryString,
+    ISearchResult
+} from './interfaces/search.interface'
+export {
+    ICertificate,
+    IEducation,
+    IExperience,
+    ILanguage,
+    ISellerDocument
+} from './interfaces/seller.interface'
+export { uploads, videoUpload } from './cloudinary-upload'
+export {
+    CustomError,
+    IError,
+    IErrorResponse,
+    ErrnoException,
+    BadRequestError,
+    NotAuthorizedError,
+    FileTooLargeError,
+    NotFoundError,
+} from './error-handler'
+export { verifyGatewayRequest } from './gateway-middleware'
+export { winstonLogger } from './logger'
+export {
+    firstLetterUppercase,
+    lowerCase,
+    toUpperCase,
+    isDataURL,
+    isEmail
+} from './helpers'
